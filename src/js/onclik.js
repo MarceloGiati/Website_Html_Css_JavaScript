85781c22a850145896c6909676eb1191bab7b28c
Marcelo Giati <mtgiati@gmail.com>
2022-11-11 16:25:41 -0300

    modif.2 ramo1

---
+++ src/js/onclik.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuIcon = document.querySelector('.menuIcon');
+    const navbar =document.getElementById('navbar');
+    menuIcon.classList.toggle('active');
+    navbar.classList.toggle('active')
+}
